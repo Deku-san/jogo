6a5aec6680301d93fed0ef46997692e603f7cea9
Deku-san <30601790+Deku-san@users.noreply.github.com>
2017-09-25 13:49:56 -0300

    Update Jogo.js

--- Jogo.js
+++ Jogo.js
@@ -41,13 +41,13 @@ function keyPressed() {
 	    x-=40
 	}
     }
-    if ((keyCode === RIGHT_ARROW) && x<=560)){
+    if ((keyCode === RIGHT_ARROW) && x<=560){
     	x+=40;
     }
-    if ((keyCode === UP_ARROW)) && y>=90)){
+    if ((keyCode === UP_ARROW) && y>=90){
     	y-=40;
     }
-    if ((keyCode === DOWN_ARROW) && y<=530)){
+    if ((keyCode === DOWN_ARROW) && y<=530){
     	y+=40;
     }
 }
