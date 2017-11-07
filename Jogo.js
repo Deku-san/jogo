93da544188ec3a783b3c4494728900c482a2318d
João Victor Santiago <30601790+Deku-san@users.noreply.github.com>
2017-11-07 11:49:29 -0200

    Add files via upload

--- Jogo.js
+++ Jogo.js
@@ -1,17 +1,42 @@
-var x = 90;
-var y = 520;
+var x = 101;
+var y = 101;
 var a = 665;
 var b = 55;
 var vidas = 100 + "%";
 var pontos = 0;
 var nivel = 1;
-var resultadoA= Math.floor(Math.random()*2)
-var resultadoB= Math.floor(Math.random()*2)
+var resultadoA= Math.floor(Math.random()*2);
+var resultadoB= Math.floor(Math.random()*2);
+var tamBloco= 202;
 function setup() {
 	createCanvas(1200, 665);
 	background(0);
 }
 
+function preload() {
+	imgParede = loadImage("tijolo.png");
+	imgTreco = loadImage("treco.png");  
+}
+
+		cenario = [ 
+  ['v', 'v', 'v', '#', '#', '#'], 
+  ['v', 'v', 'v', '#', '#', '#'],
+  ['v', 'v', 'v', '#', '#', '#'],
+]; 
+ 
+ function colisao(px, py) {
+  j = Math.floor( px / tamBloco ); 
+  i = Math.floor( py / tamBloco );
+  if ( cenario[i][j] == '#' ) {
+     return true;     
+  }
+  else {
+     return false;  
+  }
+  
+}
+ 
+ 
 function draw() {
 	background(0);
 	textSize(32);
@@ -19,39 +44,54 @@ function draw() {
 	text("Vida: " + vidas, 10, 650);
 	text("Pontos: " + pontos, 1060, 650);
 	text("Nível: " + nivel, 550, 650);
-	frameRate(1)
+	frameRate(1);
 	resultadoA= Math.floor(Math.random()*2);
 	resultadoB= Math.floor(Math.random()*2);
 		
-		cenario = [ 
-  ['v','v','v','#','#','#'], 
-  ['v','v','v','#','#','#'],
-  ['v','v','v','#','#','#'],
- 
-]; 
- 
+
+	
+	for ( i = 0; i < cenario.length; i++ ) { 
+		for ( j = 0; j < cenario[0].length; j++ ) {  
+			if ( cenario[i][j] == 'v' ) {
+				image(imgParede,j*tamBloco,i*tamBloco); 
+				fill(0);
+				stroke(0,0,255);
+				rect(j*tamBloco,i*tamBloco,tamBloco,tamBloco); 
+			}
+		}
+	}
+	for ( i = 0; i < cenario.length; i++ ) { 
+		for ( j = 0; j < cenario[0].length; j++ ) {  
+			if ( cenario[i][j] == '#' ) {
+				image(imgTreco,j*tamBloco,i*tamBloco); 
+				fill(0);
+				stroke(255,0,0);
+				rect(j*tamBloco,i*tamBloco,tamBloco,tamBloco); 
+			}
+		}
+	}
 	
 
 	if(a<950){
-	if(resultadoA=1){
-		a+=205
-	}
+		if(resultadoA=1){
+			a+=205;
+		}
 	}
 	if(a>665){
-	if(resultadoA=0){
-		a-=155
-	}
+		if(resultadoA=0){
+			a-=155;
+		}
 	}
 	if(b<305){
-	if(resultadoB=1){
-		b+=205
-	}
+		if(resultadoB=1){
+			b+=205;
+		}
 	}
 	if(b>55){
-	if(resultadoB==0){
-		b-=205
+		if(resultadoB==0){
+			b-=205;
+		}
 	}
-}
 	
 	//Protagonista
   fill(124,124,255);
@@ -65,41 +105,30 @@ function draw() {
   
   //Divisória
   fill(0,255,0);
-  rect(600,0,10,600);
-  
-  //Área do protagonista
-  fill(0,0,255);
-  rect(0,400,600,10);
-  rect(0,200,600,10);
-  rect(190,0,10,600);
-  rect(390,0,10,600);
-  
-  //Área do antagonista
-  fill(255,0,0);
-  rect(1000,0,10,600);
-  rect(800,0,10,600);
-  rect(610,400,600,10);
-  rect(610,200,600,10);
+  rect(600,0,10,607);
 
 }
 
 function keyPressed() {
 	
-	for (i=90; i <= 520; i++ ) { 
-    for (j=520; j <= 3000 ; j++ ) {  
-      if ( cenario[i][j] == 'v' ) {
-        if(keyCode === LEFT_ARROW){
-			j= j-1
+	for (i=1; i < 4; i++) { 
+    	for (j=1; j < 4; j++) {  
+      		if ( cenario[i][j] == 'v' ) {
+        		if(keyCode === LEFT_ARROW){
+					j = j-1;
+					x-= 101;	
 				}	
-		if(keyCode === RIGHT_ARROW){
-			j= j+1
-
+				if(keyCode === RIGHT_ARROW){
+					j = j+1;
+					x+= 101;
 				}
-		if (keyCode === UP_ARROW){
-			i=i-1
+				if (keyCode === UP_ARROW){
+					i = i-1;
+					y-= 101;
 				}
-		if (keyCode === DOWN_ARROW){
-			j=j+1
+				if (keyCode === DOWN_ARROW){
+					i = i+1;
+					y+= 101;
 				}
 			}
 		}
