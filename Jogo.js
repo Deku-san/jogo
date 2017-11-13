6636a9fa9672ad0622265e357f2b7d03c20b3f64
João Victor Santiago <30601790+Deku-san@users.noreply.github.com>
2017-11-13 15:52:56 -0200

    Add files via upload

--- Jogo.js
+++ Jogo.js
@@ -1,8 +1,8 @@
-var x = 101;
-var y = 101;
+var posXbola = 101;
+var posYbola = 101;
 var a = 202*3 + 70;
 var b = 101 -40;
-var vidas = 100 + "%";
+var vidas = 100;
 var pontos = 0;
 var nivel = 1;
 var resultadoA= Math.floor(Math.random()*2);
@@ -10,9 +10,22 @@ var resultadoB= Math.floor(Math.random()*2);
 var tamBloco= 202;
 var posAtiro = a
 var posBtiro = b+35
+var velIn = 100
+var raio = 40
+var perdeuVida = 10
+var vetorX = []
+var vetorY = []
+var i=0
+var j=0
 function setup() {
 	createCanvas(1200, 665);
 	background(0);
+	for(i=0; i<=10; i++){
+		vetorX[i] = 101 + 202*(Math.floor(random(0,2)))
+	}
+	for(j=0; j<=10; j++){
+		vetorY[j] = 101 + 202*(Math.floor(random(0,2)))
+	}
 }
 
 function preload() {
@@ -26,20 +39,39 @@ function preload() {
   ['v', 'v', 'v', '#', '#', '#'],
 ]; 
 
-function tiro(){
-	
+function Item(cx,cy){
+	ellipse(cx,cy, 20, 20);
 }
+
+function tiro(){
+	//Tiro
+	fill(255,0,0);
+	rect(posAtiro, posBtiro, 10, 10);
+	posAtiro= posAtiro - velIn
+	if(posAtiro < - Math.random()*(400)-400 || posAtiro == 101){
+		posAtiro = a
+		posBtiro = b + 40
+	}
+		if((posAtiro < posXbola+40) && (posAtiro > posXbola-40) && (posBtiro > posYbola -40) && (posBtiro < posYbola + 40)){
+		posAtiro = a
+		posBtiro = b + 40
+		vidas = vidas - 10
+		}
+}
+
+
 
 function draw() {
 	background(0);
 	textSize(32);
  	fill(255,255,255);
-	text("Vida: " + vidas, 10, 650);
+	text("Vida: " + vidas + "%", 10, 650);
 	text("Pontos: " + pontos, 1060, 650);
 	text("Nível: " + nivel, 550, 650);
-	frameRate(2);
+	frameRate(5);
 	resultadoA= Math.floor(Math.random()*2);
 	resultadoB= Math.floor(Math.random()*2);
+		
 		
 
 	
@@ -67,11 +99,7 @@ function draw() {
 	//Protagonista
 	fill(124,124,255);
 	stroke(255, 0, 0);
-	ellipse(x, y, 80, 80);
-	
-	//Tiro
-	fill(255);
-	rect(posAtiro, posBtiro, 10, 10);
+	ellipse( posXbola ,posYbola, 80, 80);
   
 	//Antagonista
 	noStroke();
@@ -98,42 +126,55 @@ function draw() {
 			b += 202;
 		}
 	}
-				}
+		
 	if( resultadoB == 0 ){
 		if( b > 200 ){
 			b -= 202;
 		}
-	if(posAtiro > 0){
-		posAtiro - 70
+	}
+	if(vidas==0){
+		vidas = 100
+	}
+	if(vetorX[i] == posXbola && vetorY[j] == posYbola){
+		vidas += 10	
+		vetorX[i] = 101 + 202*(Math.floor(random(0,2)))
+		vetorY[j] = 101 + 202*(Math.floor(random(0,2)))
+		if( vidas >= 100){
+			vidas = 100
 	}
 }
+	Item(vetorX[i],vetorY[j])
+	tiro()
+}
+
+	
 function keyPressed() {
 	
 	for (i=1; i < 4; i++) { 
     	for (j=1; j < 4; j++) {  
       		if ( cenario[i][j] == 'v' ) {
         		if(keyCode === LEFT_ARROW){
-					if( x > 200 ){
+					if( posXbola > 200 ){
 						j -= 1/2;
-						x -= 101;
+						posXbola -= 101;
 					}
 				}
 				if(keyCode === RIGHT_ARROW){
-					if( x <= 500 ){
+					if( posXbola <= 500 ){
 						j += 1;
-						x += 101;
+						posXbola += 101;
 					}
 				}
 				if (keyCode === UP_ARROW){
-					if( y >= 200 ){
+					if( posYbola >= 200 ){
 						i -= 1;
-						y -= 101;
+						posYbola -= 101;
 					}
 				}
 				if (keyCode === DOWN_ARROW){
-					if( y <= 500 ){
+					if( posYbola <= 500 ){
 						i += 1;
-						y += 101;
+						posYbola += 101;
 					}
 				}
 			}
